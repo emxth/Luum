1a168efc68e10f9105369c83a9950851a24793c0
emxth <randivemith.2003@gmail.com>
2026-06-07 15:23:33 +0530

    Add TransactionRepository to manage transaction data operations

---
+++ lib/data/repositories/transaction_repository.dart
@@ -0,0 +1,29 @@
+import 'package:drift/drift.dart';
+
+import '../database/app_database.dart';
+
+class TransactionRepository {
+  final AppDatabase database;
+
+  TransactionRepository(this.database);
+
+  Future<List<Transaction>> getAllTransactions() {
+    return (database.select(
+      database.transactions,
+    )..orderBy([(t) => OrderingTerm.desc(t.date)])).get();
+  }
+
+  Future<void> createTransaction(TransactionsCompanion transaction) async {
+    await database.into(database.transactions).insert(transaction);
+  }
+
+  Future<void> updateTransaction(Transaction transaction) async {
+    await database.update(database.transactions).replace(transaction);
+  }
+
+  Future<void> deleteTransaction(String id) async {
+    await (database.delete(
+      database.transactions,
+    )..where((tbl) => tbl.id.equals(id))).go();
+  }
+}
