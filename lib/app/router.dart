c17da65d8c472be1f75ac98102feac6512e3349a
emxth <randivemith.2003@gmail.com>
2026-06-07 15:24:20 +0530

    Add route for adding transactions in the router configuration

--- lib/app/router.dart
+++ lib/app/router.dart
@@ -1,5 +1,6 @@
 import 'package:go_router/go_router.dart';
 
+import '../features/transactions/presentation/add_transaction_screen.dart';
 import '../features/dashboard/presentation/dashboard_screen.dart';
 
 final router = GoRouter(
@@ -8,6 +9,13 @@ final router = GoRouter(
       path: '/',
       builder: (context, state) {
         return const DashboardScreen();
+      },
+    ),
+
+    GoRoute(
+      path: '/transactions/add',
+      builder: (context, state) {
+        return const AddTransactionScreen();
       },
     ),
   ],
