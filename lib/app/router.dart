53dfe215457770e551442bfc729ff6e8d8d1b7fb
emxth <randivemith.2003@gmail.com>
2026-06-08 14:45:48 +0530

    Add route for transaction details screen

--- lib/app/router.dart
+++ lib/app/router.dart
@@ -2,6 +2,7 @@ import 'package:go_router/go_router.dart';
 
 import '../features/transactions/presentation/add_transaction_screen.dart';
 import '../features/transactions/presentation/transaction_list_screen.dart';
+import '../features/transactions/presentation/transaction_details_screen.dart';
 import '../features/dashboard/presentation/dashboard_screen.dart';
 
 final router = GoRouter(
@@ -26,6 +27,25 @@ final router = GoRouter(
       path: '/transactions',
       builder: (context, state) {
         return const TransactionListScreen();
+      },
+    ),
+
+    GoRoute(
+      path: '/transactions/details/:id',
+      builder: (context, state) {
+        return TransactionDetailsScreen(
+          transactionId: state.pathParameters['id']!,
+        );
+      },
+    ),
+
+    GoRoute(
+      path: '/transactions/edit/:id',
+      builder: (context, state) {
+        return AddTransactionScreen(
+          transactionId: state.pathParameters['id'],
+          transactionType: 'expense',
+        );
       },
     ),
   ],
