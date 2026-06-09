b3bb0cb8220674520e25c4aa66c3706305741522
emxth <randivemith.2003@gmail.com>
2026-06-09 11:30:00 +0530

    Add DashboardSummary model to encapsulate financial summary data

---
+++ lib/features/dashboard/models/dashboard_summary.dart
@@ -0,0 +1,18 @@
+class DashboardSummary {
+  final double totalIncome;
+  final double totalExpense;
+  final double balance;
+
+  final double monthIncome;
+  final double monthExpense;
+  final double monthBalance;
+
+  DashboardSummary({
+    required this.totalIncome,
+    required this.totalExpense,
+    required this.balance,
+    required this.monthIncome,
+    required this.monthExpense,
+    required this.monthBalance,
+  });
+}
