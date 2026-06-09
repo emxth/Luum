f8bfb4ef2652f0f594290217366839580122c68a
emxth <randivemith.2003@gmail.com>
2026-06-09 14:32:54 +0530

    Add GoalDashboard model to represent goal statistics

---
+++ lib/features/goals/models/goal_dashboard.dart
@@ -0,0 +1,16 @@
+class GoalDashboard {
+  final int totalGoals;
+
+  final int activeGoals;
+
+  final int completedGoals;
+
+  final double totalSaved;
+
+  GoalDashboard({
+    required this.totalGoals,
+    required this.activeGoals,
+    required this.completedGoals,
+    required this.totalSaved,
+  });
+}
