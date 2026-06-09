2ec81836b0654288efbe4a4d9925cb52e6715ef2
emxth <randivemith.2003@gmail.com>
2026-06-09 14:30:53 +0530

    Add isCompleted field to GoalDetails model

--- lib/features/goals/models/goal_details.dart
+++ lib/features/goals/models/goal_details.dart
@@ -9,10 +9,13 @@ class GoalDetails {
 
   final double progress;
 
+  final bool isCompleted;
+
   GoalDetails({
     required this.goal,
     required this.balance,
     required this.remaining,
     required this.progress,
+    required this.isCompleted,
   });
 }
