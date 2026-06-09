88cb43dd4e9a8d6dcffc5efab9767663d51b38ca
emxth <randivemith.2003@gmail.com>
2026-06-09 11:30:19 +0530

    Add dashboardProvider to fetch and encapsulate financial summary data

---
+++ lib/features/dashboard/providers/dashboard_provider.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../data/providers/transaction_provider.dart';
+import '../models/dashboard_summary.dart';
+
+final dashboardProvider = FutureProvider((ref) async {
+  final repository = ref.read(transactionRepositoryProvider);
+
+  final income = await repository.getTotalIncome();
+
+  final expense = await repository.getTotalExpense();
+
+  final balance = income - expense;
+
+  final monthIncome = await repository.getCurrentMonthIncome();
+
+  final monthExpense = await repository.getCurrentMonthExpense();
+
+  final monthBalance = await repository.getCurrentMonthBalance();
+
+  return DashboardSummary(
+    totalIncome: income,
+    totalExpense: expense,
+    balance: balance,
+    monthIncome: monthIncome,
+    monthExpense: monthExpense,
+    monthBalance: monthBalance,
+  );
+});
