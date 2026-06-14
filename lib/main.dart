6635302c849fa2d44c7cad5703353d0322fe4426
emxth <randivemith.2003@gmail.com>
2026-06-14 16:22:54 +0530

    Refactor main function to initialize NotificationService asynchronously

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'app/app.dart';
+import 'data/services/notification_service.dart';
 
-void main() {
-  runApp(
-    const ProviderScope(
-      child: LuumApp(),
-    ),
-  );
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await NotificationService.initialize();
+
+  runApp(const ProviderScope(child: LuumApp()));
 }
