6828e1d0a95bb842ffc85ff2c525e92dbc437ba8
emxth <randivemith.2003@gmail.com>
2026-06-18 15:10:47 +0530

    Add monthName function to retrieve month names by index

---
+++ lib/features/reports/utils/month_helper.dart
@@ -0,0 +1,19 @@
+String monthName(int month) {
+  const months = [
+    '',
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
+
+  return months[month];
+}
