56841a9774a6ae182274f596376ce054e8bc2e82
emxth <randivemith.2003@gmail.com>
2026-06-18 15:10:18 +0530

    Add BestWorstMonthsModel class to represent best and worst financial months

---
+++ lib/features/reports/models/best_worst_months_model.dart
@@ -0,0 +1,16 @@
+class BestWorstMonthsModel {
+  final int bestIncomeMonth;
+
+  final int worstExpenseMonth;
+
+  final int highestExpenseMonth;
+
+  final int bestSavingsMonth;
+
+  const BestWorstMonthsModel({
+    required this.bestIncomeMonth,
+    required this.worstExpenseMonth,
+    required this.highestExpenseMonth,
+    required this.bestSavingsMonth,
+  });
+}
