55255bc74e40d86c50cd9b04b5aca28bdc701cdf
emxth <randivemith.2003@gmail.com>
2026-06-18 13:40:00 +0530

    Add YearlyMonthModel class for yearly financial reporting

---
+++ lib/features/reports/models/yearly_month_model.dart
@@ -0,0 +1,11 @@
+class YearlyMonthModel {
+  final int month;
+  final double income;
+  final double expense;
+
+  const YearlyMonthModel({
+    required this.month,
+    required this.income,
+    required this.expense,
+  });
+}
