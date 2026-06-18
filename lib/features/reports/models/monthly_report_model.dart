fda7f05d0cfd7e3ea858f97e3a3d128f84403ad7
emxth <randivemith.2003@gmail.com>
2026-06-18 13:39:21 +0530

    Add MonthlyReportModel class for monthly financial reporting

---
+++ lib/features/reports/models/monthly_report_model.dart
@@ -0,0 +1,11 @@
+class MonthlyReportModel {
+  final double income;
+  final double expense;
+  final double balance;
+
+  const MonthlyReportModel({
+    required this.income,
+    required this.expense,
+    required this.balance,
+  });
+}
