6ef2bc6dc0e85dea2d2982b8c5efc8b3b84b8e21
emxth <randivemith.2003@gmail.com>
2026-06-18 15:09:49 +0530

    Add YearSummaryModel class for annual financial summary data

---
+++ lib/features/reports/models/year_summary_model.dart
@@ -0,0 +1,16 @@
+class YearSummaryModel {
+  final double income;
+
+  final double expense;
+
+  final double balance;
+
+  final double savingsRate;
+
+  const YearSummaryModel({
+    required this.income,
+    required this.expense,
+    required this.balance,
+    required this.savingsRate,
+  });
+}
