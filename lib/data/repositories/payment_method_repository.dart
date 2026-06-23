0e09ce05a09406791446ba273e80efce171b9e05
emxth <randivemith.2003@gmail.com>
2026-06-23 12:05:10 +0530

    Add payment method management methods in PaymentMethodRepository

--- lib/data/repositories/payment_method_repository.dart
+++ lib/data/repositories/payment_method_repository.dart
@@ -8,4 +8,38 @@ class PaymentMethodRepository {
   Future<List<PaymentMethod>> getAllPaymentMethods() {
     return database.select(database.paymentMethods).get();
   }
+
+  Future<void> createPaymentMethod(PaymentMethodsCompanion method) async {
+    await database.into(database.paymentMethods).insert(method);
+  }
+
+  Future<void> updatePaymentMethod(PaymentMethod method) async {
+    await database.update(database.paymentMethods).replace(method);
+  }
+
+  Future<void> deletePaymentMethod(String id) async {
+    await (database.delete(
+      database.paymentMethods,
+    )..where((tbl) => tbl.id.equals(id))).go();
+  }
+
+  Future<bool> isPaymentMethodUsed(String paymentMethodId) async {
+    final result = await (database.select(
+      database.transactions,
+    )..where((tbl) => tbl.paymentMethodId.equals(paymentMethodId))).get();
+
+    return result.isNotEmpty;
+  }
+
+  Future<bool> deletePaymentMethodSafe(String id) async {
+    final used = await isPaymentMethodUsed(id);
+
+    if (used) {
+      return false;
+    }
+
+    await deletePaymentMethod(id);
+
+    return true;
+  }
 }
